d0e8849a0061e79ea73103630d8f323f8c952f1c
Mishok1 <mm21112000@gmail.com>
2024-02-15 15:08:24 +0300

    Fixs counter

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -46,6 +46,37 @@ const swiper = new Swiper('.swiper', {
 
 //counter
 
+// const deadline = 'February 16 2024 23:59:59 GMT+0200'
+
+// function getTimeRemaining(endtime){
+//   const total = Date.parse(endtime) - Date.parse(new Date());
+//   const seconds = Math.floor( (total/1000) % 60 );
+//   const minutes = Math.floor( (total/1000/60) % 60 );
+//   const hours = Math.floor( (total/(1000*60*60)) % 24 );
+//   const days = Math.floor( total/(1000*60*60*24) );
+
+//   return {
+//     total,
+//     days,
+//     hours,
+//     minutes,
+//     seconds
+//   };
+// }
+
+// function initializeClock(endtime) {
+//   // const clock = document.getElementById(id);
+//   const timeinterval = setInterval(() => {
+//     const t = getTimeRemaining(endtime);
+//     document.getElementById('hours').innerText = t.days;
+//     document.getElementById('minutes').innerText = t.minutes;
+//     document.getElementById('seconds').innerText = t.seconds;
+//   },1000);
+// }
+
+// initializeClock(deadline);
+
+
 const deadline = 'February 16 2024 23:59:59 GMT+0200'
 
 function getTimeRemaining(endtime){
@@ -68,9 +99,19 @@ function initializeClock(endtime) {
   // const clock = document.getElementById(id);
   const timeinterval = setInterval(() => {
     const t = getTimeRemaining(endtime);
-    document.getElementById('hours').innerText = t.days;
-    document.getElementById('minutes').innerText = t.minutes;
-    document.getElementById('seconds').innerText = t.seconds;
+
+    const hoursStr = t.hours < 10? `0${t.hours}` : `${t.hours}`
+    const minutesStr = t.minutes < 10? `0${t.minutes}` : `${t.minutes}`
+    const secondsStr = t.seconds < 10? `0${t.seconds}` : `${t.seconds}`
+
+    const hoursHTML = `<span class='num'>${hoursStr[0]}</span> <span class='num'>${hoursStr[1]}</span>`
+    const minutesHTML = `<span class='num'>${minutesStr[0]}</span> <span class='num'>${minutesStr[1]}</span>`
+    const secondsHTML = `<span class='num'>${secondsStr[0]}</span> <span class='num'>${secondsStr[1]}</span>`
+
+
+    document.getElementById('hours').innerHTML = hoursHTML
+    document.getElementById('minutes').innerHTML = minutesHTML
+    document.getElementById('seconds').innerHTML = secondsHTML
   },1000);
 }
 
