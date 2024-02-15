c03ade898dd3078e7ca489a99303042b5c442d6a
Mishok1 <mm21112000@gmail.com>
2024-02-15 21:52:33 +0300

    Fixs stack

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,21 +1,15 @@
-// core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// init Swiper:
-// configure Swiper to use modules
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   modules: [Navigation, Pagination],
   loop: true,
   draggable: true,
 
-  // If we need pagination
   pagination: {
     enabled: true,
     clickable: true,
@@ -23,19 +17,11 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
   },
 
-  // pagination: {
-  //   el: '.swiper-pagination',
-  //   clickable: true,
-  // },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  // scrollbar: none,
   scrollbar: {
     el: '.swiper-scrollbar',
     hide: true,
@@ -43,39 +29,7 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-
 //counter
-
-// const deadline = 'February 16 2024 23:59:59 GMT+0200'
-
-// function getTimeRemaining(endtime){
-//   const total = Date.parse(endtime) - Date.parse(new Date());
-//   const seconds = Math.floor( (total/1000) % 60 );
-//   const minutes = Math.floor( (total/1000/60) % 60 );
-//   const hours = Math.floor( (total/(1000*60*60)) % 24 );
-//   const days = Math.floor( total/(1000*60*60*24) );
-
-//   return {
-//     total,
-//     days,
-//     hours,
-//     minutes,
-//     seconds
-//   };
-// }
-
-// function initializeClock(endtime) {
-//   // const clock = document.getElementById(id);
-//   const timeinterval = setInterval(() => {
-//     const t = getTimeRemaining(endtime);
-//     document.getElementById('hours').innerText = t.days;
-//     document.getElementById('minutes').innerText = t.minutes;
-//     document.getElementById('seconds').innerText = t.seconds;
-//   },1000);
-// }
-
-// initializeClock(deadline);
-
 
 const deadline = 'February 16 2024 23:59:59 GMT+0200'
 
@@ -96,7 +50,6 @@ function getTimeRemaining(endtime){
 }
 
 function initializeClock(endtime) {
-  // const clock = document.getElementById(id);
   const timeinterval = setInterval(() => {
     const t = getTimeRemaining(endtime);
 
