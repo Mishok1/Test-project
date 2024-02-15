626580a4c4a6be7fb7182d5f825308bef6fb15e3
Mishok1 <mm21112000@gmail.com>
2024-02-15 14:37:46 +0300

    Adds another counter

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -42,3 +42,36 @@ const swiper = new Swiper('.swiper', {
     enabled: false,
   },
 });
+
+
+//counter
+
+const deadline = 'February 16 2024 23:59:59 GMT+0200'
+
+function getTimeRemaining(endtime){
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(endtime) {
+  // const clock = document.getElementById(id);
+  const timeinterval = setInterval(() => {
+    const t = getTimeRemaining(endtime);
+    document.getElementById('hours').innerText = t.days;
+    document.getElementById('minutes').innerText = t.minutes;
+    document.getElementById('seconds').innerText = t.seconds;
+  },1000);
+}
+
+initializeClock(deadline);
